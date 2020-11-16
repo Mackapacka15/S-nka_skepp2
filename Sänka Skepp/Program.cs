5515e0433e4b0108d1e139d0577bb09f258388a0
Markus <Markuspiotrowski@live.com>
2020-11-16 08:25:46 +0100

    Sänka_Skepp2

---
+++ Sänka Skepp/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Raylib_cs;
+using System.Collections.Generic;
+
+namespace Sänka_Skepp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Raylib.InitWindow(500, 500, "Sänka Skepp");
+            Raylib.SetTargetFPS(60);
+            int height = Raylib.GetScreenHeight();
+            int width = Raylib.GetScreenWidth();
+            string state = "menu";
+            int distance = 100;
+            int[] playerBoats = new int[4];
+            while (!Raylib.WindowShouldClose())
+            {
+
+
+
+
+
+                Raylib.BeginDrawing();
+                Raylib.ClearBackground(Color.BLACK);
+
+                if (state == "menu")
+                {
+                    Raylib.DrawText("Welcome to Battleship", width / 2 - 130, 200, 25, Color.WHITE);
+                    Raylib.DrawText("Press enter to continue", width / 2 - 125, 250, 20, Color.WHITE);
+                    Raylib.DrawText("Press H for rules", width / 2 - 110, 300, 20, Color.WHITE);
+                    if (Raylib.IsKeyPressed(KeyboardKey.KEY_ENTER))
+                    {
+                        state = "game";
+                    }
+                }
+                if (state == "game")
+                {
+                    for (int i = 0; i < 6; i++)
+                    {
+                        Raylib.DrawLine(i * distance, 1, i * distance, 500, Color.WHITE);
+                        Raylib.DrawLine(1, i * distance, 500, i * distance, Color.WHITE);
+                    }
+                }
+
+                Raylib.EndDrawing();
+            }
+        }
+    }
+}
